ba2bd645830cea9310515b19beae845a0f721d55
James Santangelo <james.santangelo37@gmail.com>
2017-06-20 17:12:27 -0400

    Modified so that data is written every iteration then deleted. Also now prints a progress bar to the command line under the PID and parameter combinations for the current simulation.

--- SEC_Code/Python/Simulate.py
+++ SEC_Code/Python/Simulate.py
@@ -2,6 +2,7 @@
 
 from Functions import *
 from Parameters import *
+from tqdm import *
 
 def simulate():
 	'''Generate 'sims' simulations of 'cline' function.
@@ -19,38 +20,46 @@ def simulate():
 	'''
 	# Initilize 'sim' dictionary to store results and 'Distance_Dic' dictionary
 	# to store distances and migration rates for simulations.
-	sim = {}
 	Distance_Dic = Distance_Mig(x_mat, y_mat, max_mig_rate)
 
-	# Loop for 'sims' iterations
-	for s in range(sims):
+	print os.getpid(), (max_mig_rate, bot)
 
-		# Initialize pop_counter, 'pops' and 'alleles' dictionaries and
-		# Matrix, with population 1 initializes in Matrix
-		pop_counter = [1]
-		pops = OrderedDict({'1':[]}) # Re-initialize dictionary to store populations
-		alleles = OrderedDict({'1':{'A':[],'B':[],'S':[N]}}) # Re-initialize dictionary to store allele lists
-		Matrix = np.zeros((x_mat, y_mat), dtype = 'int')
-		Matrix[0, 0] = 1
-
-		# Initialize allele lists from which population 1 will sample its
-		# alleles in generation 1.
-		locus_A = (['A'] * int(N * pA) ) + (['a'] * int(round(N * qA)))  # Re-initialize initial allele lists.
-		locus_B = (['B'] * int(N * pB) ) + (['b'] * int(round(N * qB)))
-
-		# Run 'cline' function.
-		cline(locus_A,locus_B, steps, N, max_p_create, pops, alleles, bot, Matrix, K, r, max_mig_rate, pop_counter, Distance_Dic) # Run cline function
-
-		# Append results from 'cline' function 'sim' dictionary as new entry.
-		# Keys correspond to interation.
-		sim[s] = pops # Append results to 'sim' dictionary
-
-	# Changed directory and write results to csv.
+	# Open csv to which data will be written
 	os.chdir(export_path)
-	write_to_csv(max_mig_rate, bot, sim)
+	datestring = datetime.strftime(datetime.now(), '%Y%m%d')
+	with open(datestring + "_SEC_Drift.Migration_(m%.4f)(bot%.3f).csv" % (max_mig_rate, bot), "wb") as f:
+		writer = csv.writer(f)
+		writer.writerow(["Sim","x","y","Population","Pop_size","Generation","pA","pB","Acyan", "Mig_rate", "K", "r", "max_p_create", "bot", "Mat.full"])
+
+		# Loop for 'sims' iterations
+		for s in tqdm(range(sims)):
+
+			sim = {}
+
+			# Initialize pop_counter, 'pops' and 'alleles' dictionaries and
+			# Matrix, with population 1 initializes in Matrix
+			pop_counter = [1]
+			pops = OrderedDict({'1':[]}) # Re-initialize dictionary to store populations
+			alleles = OrderedDict({'1':{'A':[],'B':[],'S':[N]}}) # Re-initialize dictionary to store allele lists
+			Matrix = np.zeros((x_mat, y_mat), dtype = 'int')
+			Matrix[0, 0] = 1
+
+			# Initialize allele lists from which population 1 will sample its
+			# alleles in generation 1.
+			locus_A = (['A'] * int(N * pA) ) + (['a'] * int(round(N * qA)))  # Re-initialize initial allele lists.
+			locus_B = (['B'] * int(N * pB) ) + (['b'] * int(round(N * qB)))
+
+			# Run 'cline' function.
+			cline(locus_A,locus_B, steps, N, max_p_create, pops, alleles, bot, Matrix, K, r, max_mig_rate, pop_counter, Distance_Dic) # Run cline function
+
+			# Append results from 'cline' function 'sim' dictionary as new entry.
+			# Keys correspond to interation.
+			sim[s] = pops # Append results to 'sim' dictionary
+
+			write_to_csv(writer, sim)
+
+			# Ensure sim is deleted to avoid overconsumption of RAM
+			del sim
 
 if __name__ == '__main__':
-	simulate() # Call function if script is run as main.
-
-
-
+	simulate()
