e2d155f6bfec865335aa52aaaf093dc8d87d8caa
James Santangelo <james.santangelo37@gmail.com>
2017-06-23 18:34:31 -0700

    Forced all calls to population number to integer rather than string

--- SEC_Code/Python/Functions.py
+++ SEC_Code/Python/Functions.py
@@ -200,7 +200,7 @@ def alleles_next_gen(Akey, pop_list, alleles, Matrix, Distance_Dic):
 	1. pA1: Probability of sampling 'A' allele in next generation.
 	2. pA2: Probability of sampling 'B' allele in next generation.
 	'''
-	to_pop = [(i, sublist.index(Akey)) for i, sublist in enumerate(Matrix) if Akey in sublist][0] # Location of focal population in matrix.
+	to_pop = [(i, sublist.index(int(Akey))) for i, sublist in enumerate(Matrix) if int(Akey) in sublist][0] # Location of focal population in matrix.
 	migration_weighted = [] # List holding migration rates
 	allele_weighted_A = [] # List holding frequency of 'A' alleles
 	allele_weighted_B = [] # List holding frequency of 'B' alleles
@@ -209,7 +209,7 @@ def alleles_next_gen(Akey, pop_list, alleles, Matrix, Distance_Dic):
 		if Akey == i:
 			pass
 		else:
-			from_pop = [(j, sublist.index(i)) for j, sublist in enumerate(Matrix) if i in sublist][0] # Location of source population in matrix
+			from_pop = [(j, sublist.index(int(i))) for j, sublist in enumerate(Matrix) if int(i) in sublist][0] # Location of source population in matrix
 			con = str(to_pop) + '.' + str(from_pop) # Create concatenated string from current focal population and source population
 			migration_weighted.append(Distance_Dic[con][1]) # Append migration rate to list
 			allele_weighted_A.append(allele_freq(alleles[i]['A'])) # Append alleles 'A' frequency to list
@@ -360,7 +360,6 @@ def create_population(max_p_create, K, Akey, Avalue, pops, alleles, bot, Matrix,
 					(0 <= x2 <= X) and
 					(0 <= y2 <= Y))]
 			# Reduced list containing only neighboring cells that lack a population
-			print Matrix
 			Nlist_red = []
 			for item in Nlist:
 				i, j = item[0], item[1]
