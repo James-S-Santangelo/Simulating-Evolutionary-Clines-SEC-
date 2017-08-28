89341284ce678c2891c8288b6b4051a06a6a9b3f
James Santangelo <james.santangelo37@gmail.com>
2017-08-28 18:47:55 -0400

    Added docstrings to Cell.py and Test_Cell.py

--- SEC_Code/Python/PyPy_OOP/Fill_one_cell/Cell.py
+++ SEC_Code/Python/PyPy_OOP/Fill_one_cell/Cell.py
@@ -6,15 +6,31 @@ import Functions
 
 
 class Cell(object):
+    """Controls cells in the landscape matrix"""
+
     def __init__(self, i, j, pop):
+        """Class constructor function
+
+        Args:
+            i (int): Row number of cell in landscape matrix
+            j (int): Column nujmber of cell in landscape matrix
+            Pop (bool): True if cell contains population. False if no population
+        """
         self.i = i  # Rows
         self.j = j  # Columns
-
         self.pop = False
-        # self.population = Population.Population(0, [], [])
 
     @classmethod
     def initialize_matrix(cls, num_cols, num_rows):
+        """Creates the landscape matrix and intantiates each cell with a Cell object
+
+        Args:
+            num_cols (int): Number of columns in landscape
+            num_rows (int): Number of rows in landscape
+
+        Returns:
+            Matrix (:obj:'list' of :obj:'int'): 2D array storing instance of Cell at every position
+        """
         Matrix = [[0] * num_cols for _ in range(num_rows)]
         for i in range(num_rows):
             for j in range(num_cols):
@@ -22,6 +38,22 @@ class Cell(object):
         return Matrix
 
     def weight_create_prob(self, max_pop_size, max_create_prob):
+        """Weights the probability of creating a population
+
+        Based on maximum desired creation probability and a populations
+        current size, determine the weighted probability that a given
+        population will create a new one
+
+        Args:
+            max_pop_size (int): Maximum population size (i.e. carrying capacity)
+            max_create_prob (float): Maximum probability of creating a new population
+
+        Returns:
+            weighted_create_prob (float): weighted probability of creating a population
+
+        Raises:
+            ValueError: If max_create_prop is negative or greater than 1
+            """
         if 0 <= max_create_prob <= 1:
             max_create_prob = float(max_create_prob)
             slope = (max_create_prob - 0) / (max_pop_size - 0)
@@ -32,7 +64,19 @@ class Cell(object):
             raise ValueError('Creation probability out of range!')
 
     @staticmethod
-    def will_create(weighted_create_prob, possibilities):
+    def will_create(weighted_create_prob):
+        """Determine if current population will create a new one
+
+        From the weighted creation probability, determine if the
+        current population successfully creates a new one.
+
+        Args:
+            weighted_create_prob (float): Weighted probability of creating a new population
+
+        Returns:
+            bool: True if new population will be created. False if not.
+        """
+        possibilities = '10'
         weighted_create_prob = float(weighted_create_prob)
         weights = [weighted_create_prob, (1 - weighted_create_prob)]
         will_create = Functions.choice(possibilities, weights)
@@ -42,6 +86,23 @@ class Cell(object):
             return False
 
     def empty_neighbors(self, num_rows, num_cols, Matrix):
+        """Checks all adjacent cells in landscape matrix for populations
+
+        Check whether adjacent cells in the landscape matrix contains populations,
+        ignoring boundaries.
+
+        Args:
+            num_rows (int): Number of rows in landscape matrix
+            num_cols (int): Number of columns in landscape matrix
+            Matrix (:obj:'list' of :obj:'int'): 2D array storing instance of Cell at every position.
+
+        Returns:
+            empty_neighbors (:obj:'list' of :obj:'tuple'): Tuples are positions (i, j) of adjacent
+            cells that do not contain populations
+
+        Raises:
+            Exception: If focal cell contain no population
+        """
         if self.pop:
             neighbors = [(x2, y2) for x2 in range(self.i - 1, self.i + 2)
                          for y2 in range(self.j - 1, self.j + 2)
@@ -66,8 +127,31 @@ class Cell(object):
             raise Exception('Cell without population cannot create new populations (obviously')
 
     def create_population(self, num_rows, num_cols, Matrix, max_create_prob, bot_prop, max_pop_size):
+        """Creates a population in randomly chosen, empty, adjacent cell
+
+        Creates a new population in a randomly selected adjacent cell that
+        contains no population. New populations are formed by sampling alleles
+        from the focal population creating the new one. The number of alleles
+        sampled depends on the bottleneck proportion.
+
+        Args:
+            num_rows (int): Number of rows in the landscape matrix
+            num_cols (int): Number of columns in the landscape matrix
+            Matrix (:obj:'list' of :obj:'int'): 2D array storing instance of Cell at every position.
+            max_create_prob (float): Maximum probability of creating a new population.
+            bot_prop (float): Bottleneck proportion. Proportion of alleles sampled upon creation of
+            new population.
+            max_pop_size (int): Maximum population size (i.e. carrying capacity).
+
+            Returns:
+                None: If empty_neighbors returns empty list (i.e. all adjacent cells contain populations)
+                or if will_create returns False (i.e. new population not being created).
+
+            Raises:
+                Exception: if bottleneck proportion is 0.
+        """
         weighted_create_prob = self.weight_create_prob(max_pop_size, max_create_prob)
-        will_create = self.will_create(weighted_create_prob, possibilities='10')
+        will_create = self.will_create(weighted_create_prob)
 
         if will_create:
             empty_neighbors = self.empty_neighbors(num_rows, num_cols, Matrix)
@@ -89,6 +173,21 @@ class Cell(object):
             return None
 
     def real_migration_rate(self, source_y, source_x, num_rows, num_cols, max_mig_rate):
+        """Calculates migration rate between populations
+
+        Calculates the realized migration rate between two populations, which
+        declines linearly with increasing distance between them.
+
+        Args:
+            source_y (int): Row index of source population (i.e. from which alleles are arriving)
+            source_x (int): Column index of source population (i.e. from which alleles are arriving)
+            num_rows (int): Number of rows in landscape matrix.
+            num_cols (int): Number of columns in landscape matrix
+            max_mig_rate (float): Maximum possible migration between two populations
+
+        Returns:
+            mig_prop (float): Proportion of alleles migrating between focal and source population.
+        """
         dist = (((source_y - self.i) ** 2) + ((source_x - self.j) ** 2)) ** (0.5)
         max_i = num_rows - 1
         max_j = num_cols - 1
@@ -101,6 +200,21 @@ class Cell(object):
         return round(mig_prop, 4)
 
     def real_K(self, num_rows, num_cols, min_K, max_K):
+        """Calculates carrying capacity of cell in the matrix
+
+        Calculates the relized carrying capacity of cell in the matrix, which
+        declines linearly with increasing distance from the first
+        initialized landscape cell.
+
+        Args:
+            num_rows (int): Number of rows in the landscape matrix
+            num_cols (int): Number of columns in the landscape matrix
+            min_K (int): Minimum carrying capacity at cell furthest away from start
+            max_K (int): Maximum possible carrying capacity (i.e. at start).
+
+            returns:
+                K (float): Realized carrying capacity of landscape matrix cell.
+        """
         start_y = 0
         start_x = 0
         dist = (((start_y - self.i) ** 2) + ((start_x - self.j) ** 2)) ** (0.5)
@@ -112,7 +226,23 @@ class Cell(object):
         return int(math.ceil(K))
 
     def source_population_info(self, pop_list, Matrix, num_rows, num_cols, max_mig_rate):
+        """Collects migration, allele frequency, and population size info from all source populations
 
+        Args:
+            pop_list (:obj:'list' of :obj:'tuple'): Tuples with row and columns indices of all landscape
+            cells with populations.
+            Matrix (:obj:'list' of :obj:'int'): 2D array storing instance of Cell at every position
+            num_rows (int): Number of rows in landscape matrix.
+            num_cols (int): Number of columns in landscape matrix
+            max_mig_rate (float): Maximum possible migration between two populations
+
+        Returns:
+            migration_weighted (:obj:`list` of :obj:`float`): Realized migration rates from all source
+            populations
+            allele_weighted_A (:obj:`list` of :obj:`float`): Frequency of 'A' allele in all source populations
+            allele_weighted_B (:obj:`list` of :obj:`float`): Frequency of 'B' allele in all source populations
+            pop_size (:obj:`list` of :obj:`int`): Population sizes of all source populations
+        """
         migration_weighted = []
         allele_weighted_A = []
         allele_weighted_B = []
@@ -141,10 +271,29 @@ class Cell(object):
 
     @staticmethod
     def weighting(values_list, factor_list):
-            value_weighted = sum(values_list[i] * factor_list[i] / sum(factor_list) for i in range(len(values_list)))
-            return round(value_weighted, 4)
+        """Weights a list of focal values based on list of weighting factors
+
+        Args:
+            values_list (:obj:'list' of :obj:'float'): List of elements to be weighted
+            factor_list (:obj:'list' of :obj:'int'): List of elements to be used as weighting factors
+
+        Returns:
+            value_weighted (float): Weighted value
+        """
+        value_weighted = sum(values_list[i] * factor_list[i] / sum(factor_list) for i in range(len(values_list)))
+        return round(value_weighted, 4)
 
     def freq_after_migration(self, migration_weighted, allele_weighted, locus):
+        """Calculates the frequency of an allele following migration
+
+        Args:
+            migration_weighted (float): Weighted migration rate across all source populations
+            allele_weighted (float): Weighted frequency of allele across all source populations
+            locus (:obj:'list' of :obj:'str'): List containing alleles at either 'A' or 'B' locus
+
+        Returns:
+            p_1 (float): Frequency of allele following migration
+        """
         p_0 = self.population.allele_freq(locus)
         p_1 = ((1 - migration_weighted) * p_0) + (migration_weighted * allele_weighted)
 
--- SEC_Code/Python/PyPy_OOP/Fill_one_cell/Tests/Test_Cell.py
+++ SEC_Code/Python/PyPy_OOP/Fill_one_cell/Tests/Test_Cell.py
@@ -1,5 +1,4 @@
 import unittest
-from parameterized import parameterized
 import math
 from mock import patch, Mock
 
@@ -8,11 +7,21 @@ from Population import Population
 
 
 class TestCell(unittest.TestCase):
-
-    # [[1, 1, 0],
-    #  [0, 0, 1]]
+    """Tests Cell.py"""
 
     def setUp(self):
+        """setUp to be run before each test function
+
+        Creates a landscape with 2 rows and 3 columns. Initializes
+        mocked populations of varying sizes and allele frequencies into
+        3 of the 6 landscape matrix cells, according to the following pattern
+
+        [[1, 2, 0],
+         [0, 0, 3]]
+
+        where cells that are not 0 contain populations. Numbers correspond to
+        the mocked populations created in function.
+        """
         self.num_rows = 2
         self.num_cols = 3
 
@@ -33,69 +42,88 @@ class TestCell(unittest.TestCase):
         self.Matrix[0][1].population = mock_pop_2
         self.Matrix[1][2].population = mock_pop_3
 
-        # print Population.allele_freq(self.Matrix[1][2].population.locus_B)
-
     def tearDown(self):
+        """tearDown function that deletes landscape matrix after each test"""
         del self.Matrix
 
     def test_initialize_matrix(self):
-
+        """Tests initialize_matrix method"""
         num_rows = 2
         num_cols = 5
         Matrix = Cell.initialize_matrix(num_cols, num_rows)
 
+        # Checks that initialize_matrix successfully instantiates Cell objects in cells.
         for i in range(num_rows):
             for j in range(num_cols):
                 assert isinstance(Matrix[i][j], Cell)
 
     def test_init(self):
+        """Tests contructor method"""
 
+        # Tests that row and column indices are successfully assigned.
         for i in range(self.num_rows):
             for j in range(self.num_cols):
                 self.assertEqual(self.Matrix[i][j].i, i)
                 self.assertEqual(self.Matrix[i][j].j, j)
 
+        # Tests that cells initialized with population have pop set to True
         self.assertIs(self.Matrix[0][0].pop, True)
         self.assertIs(self.Matrix[0][1].pop, True)
         self.assertIs(self.Matrix[1][2].pop, True)
 
+        # Tests that cells NOT initialized with population have pop set to False
         self.assertIs(self.Matrix[0][2].pop, False)
         self.assertIs(self.Matrix[1][0].pop, False)
         self.assertIs(self.Matrix[1][1].pop, False)
 
+        # Tests that one of the cells with a population is correctly assigned an instance of Population class
         assert isinstance(self.Matrix[0][0].population, Population)
+        # Tests that instance of Population class has correctly assigned size attribute.
         self.assertEqual(self.Matrix[0][0].population.size, 10)
 
     def test_weight_create_prob(self):
+        """Tests weight_create_prob method"""
 
+        # Zero returned when max_prob_create is 0
         self.assertEqual(self.Matrix[0][0].weight_create_prob(1000, 0), 0)
-        self.assertEqual(self.Matrix[0][0].weight_create_prob(1000, 0.5), 0.005)
+        # Correct computation of creation probability
+        self.assertEqual(self.Matrix[0][0].weight_create_prob(100, 0.5), 0.05)
+        # Computes current probability as max_prob when current size is equal to max_size
         self.assertEqual(self.Matrix[0][0].weight_create_prob(10, 0.5), 0.5)
+        # Correct computation when max_size is 1
         self.assertEqual(self.Matrix[0][0].weight_create_prob(1000, 1), 0.01)
 
-        # Creation probabilities above 1 raise error
+        # Raises ValueError if creation_prob is greater than 1
         self.assertRaises(ValueError, self.Matrix[0][0].weight_create_prob, 1000, 1.5)
-
-        # Negative creation probabilities default to 0
+        # Raises ValueError if creation_prob is negative
         self.assertRaises(ValueError, self.Matrix[0][0].weight_create_prob, 1000, -0.5)
-
         # Raises attribute error if function called on cell with no population
         self.assertRaises(AttributeError, self.Matrix[0][2].weight_create_prob, 1000, 1)
 
     @patch('Functions.choice')
     def test_will_create(self, mock_choice):
+        """Tests will_create method
 
+        args:
+            mock_choice (:obj:'mocked function'): Mocked version of choice functon from Functions.py
+        """
+
+        # True when weighted_create_prob is 1
         mock_choice.return_value = '1'
-        self.assertIs(self.Matrix[0][1].will_create(1, '10'), True)
+        self.assertIs(self.Matrix[0][1].will_create(1), True)
 
+        # False when weighted_create_prob is 0
         mock_choice.return_value = '0'
-        self.assertIs(self.Matrix[0][1].will_create(0, '10'), False)
+        self.assertIs(self.Matrix[0][1].will_create(0), False)
 
+        # True when weighted_create_prob is 0.5 and choice returns 1
         mock_choice.return_value = '1'
-        self.assertIs(self.Matrix[0][1].will_create(0.5, '10'), True)
+        self.assertIs(self.Matrix[0][1].will_create(0.5), True)
 
     def test_empty_neighbors(self):
+        """Tests empty_neighbors method"""
 
+        # Correctly returns empty adjacent cells. Tested on all mocked populations
         expected_list = [(1, 0), (1, 1)]
         self.assertListEqual(self.Matrix[0][0].empty_neighbors(self.num_rows, self.num_cols, self.Matrix), expected_list)
         expected_list = [(0, 2), (1, 0), (1, 1)]
@@ -103,7 +131,7 @@ class TestCell(unittest.TestCase):
         expected_list = [(0, 2), (1, 1)]
         self.assertListEqual(self.Matrix[1][2].empty_neighbors(self.num_rows, self.num_cols, self.Matrix), expected_list)
 
-        # Fails since no population in cell calling neighbors function
+        # Fails since no population in cell calling neighbors method
         self.assertRaises(Exception, self.Matrix[0][2].empty_neighbors, self.num_rows, self.num_cols, self.Matrix)
 
         # Return empty list if all adjacent cells are occupied
@@ -124,20 +152,29 @@ class TestCell(unittest.TestCase):
     @patch('Cell.Cell.empty_neighbors')
     @patch('Cell.Cell.will_create')
     @patch('Cell.Cell.weight_create_prob')
-    def test_create_population(self, mock_weighted, will_create, mock_neighbors, mock_random, mock_new_pop):
+    def test_create_population(self, mock_weighted, mock_will_create, mock_neighbors, mock_random, mock_new_pop):
+        """Tests create_population method
 
-        # Testing that None is returned if 'will_create' is False
+        Args:
+            mock_weighted (:obj:'mocked method'): Mocked weight_create_prob method
+            mock_will_create (:obj:'mocked method'): Mocked will_create method
+            mock_neighbors (:obj:'mocked method'): Mocked empty_neighbors method
+            mock_random (:obj:'mocked method'): Mocked randint method from random module
+            mock_new_pop (:obj:'mocked class'): Mocked instance of Population class
+        """
+
+        # Testing that None is returned if 'mock_will_create' is False
         mock_weighted.return_value = 0
-        will_create.return_value = False
+        mock_will_create.return_value = False
         max_create_prob = 0  # Not relevant since mocked
         max_pop_size = 100  # Not relevant since mocked
         bot_prop = 1.0
 
         self.assertIs(self.Matrix[0][0].create_population(self.num_rows, self.num_cols, self.Matrix, max_create_prob, bot_prop, max_pop_size), None)
 
-        # Testing that None is returned if 'will_create' is True but all adjacent cells contain populations (i.e. 'empty_neighbors' returns empty list)
+        # Testing that None is returned if 'mock_will_create' is True but all adjacent cells contain populations (i.e. 'empty_neighbors' returns empty list)
         mock_weighted.return_value = 1.0
-        will_create.return_value = True
+        mock_will_create.return_value = True
         mock_neighbors.return_value = []
         max_create_prob = 1.0  # Not relevant since mocked
         max_pop_size = 100  # Not relevant since mocked
@@ -147,7 +184,7 @@ class TestCell(unittest.TestCase):
 
         # Testing that Exception is raised if the bottleneck proportion is 0
         mock_weighted.return_value = 1.0
-        will_create.return_value = True
+        mock_will_create.return_value = True
         mock_neighbors.return_value = [(0, 2), (1, 1)]
         mock_random.return_value = 0
         max_create_prob = 1.0  # Not relevant since mocked
@@ -158,7 +195,7 @@ class TestCell(unittest.TestCase):
 
         # Testing that a new population with correct size and loci is created in only one adjacent cell.
         mock_weighted.return_value = 1.0
-        will_create.return_value = True
+        mock_will_create.return_value = True
         mock_neighbors.return_value = [(0, 2), (1, 1)]
         mock_random.return_value = 0
         max_create_prob = 1.0  # Not relevant since mocked
@@ -184,33 +221,41 @@ class TestCell(unittest.TestCase):
         self.assertListEqual(self.Matrix[0][2].population.locus_B, (['B'] * 4) + (['b'] * 3))
 
     def test_real_migration_rate(self):
+        """Tests real_migration_rate method"""
 
+        # Correctly returns migration rate from adjacent cell
         max_mig_rate = 0.05
         source_y = 0
-        source_x = 1
-        self.assertEqual(self.Matrix[0][0].real_migration_rate(source_y, source_x, self.num_rows, self.num_cols, max_mig_rate), 0.0276)
+        source_x = 0
+        self.assertEqual(self.Matrix[0][1].real_migration_rate(source_y, source_x, self.num_rows, self.num_cols, max_mig_rate), 0.0276)
 
+        # Correactly returns 0 when maximum migration rate is 0
         max_mig_rate = 0
         self.assertEqual(self.Matrix[0][0].real_migration_rate(source_y, source_x, self.num_rows, self.num_cols, max_mig_rate), 0)
 
+        # Correctly returns migration rate from non-adjacent cell
         max_mig_rate = 0.05
         source_y = 0
         source_x = 2
         self.assertEqual(self.Matrix[0][0].real_migration_rate(source_y, source_x, self.num_rows, self.num_cols, max_mig_rate), 0.0053)
 
+        # Correctly returns migration rate of 0 when cell is at maximum dtstance
         max_mig_rate = 0.05
         source_y = 1
         source_x = 2
         self.assertEqual(self.Matrix[0][0].real_migration_rate(source_y, source_x, self.num_rows, self.num_cols, max_mig_rate), 0)
 
     def test_real_K(self):
+        """Tests real_K function"""
 
+        # Correctly return carrying capacity for all cells with populations defined in setUp
         min_K = 10
         max_K = 1000
         self.assertEqual(self.Matrix[0][0].real_K(self.num_rows, self.num_cols, min_K, max_K), 1000)
         self.assertEqual(self.Matrix[1][2].real_K(self.num_rows, self.num_cols, min_K, max_K), 10)
         self.assertEqual(self.Matrix[0][2].real_K(self.num_rows, self.num_cols, min_K, max_K), 115)
 
+        # Correctly returns carrying capacity when K is not set to vary
         min_K = 1000
         max_K = 1000
         self.assertEqual(self.Matrix[0][0].real_K(self.num_rows, self.num_cols, min_K, max_K), 1000)
@@ -219,6 +264,11 @@ class TestCell(unittest.TestCase):
 
     @patch('Cell.Cell.real_migration_rate')
     def test_source_population_info(self, mock_real):
+        """Tests source_population_info method
+
+        Args:
+            mock_real (:onj:'Mocked method'): Mocked real_migration_rate method
+        """
 
         pop_list = [(i, j) for j in range(self.num_cols) for i in range(self.num_rows) if self.Matrix[i][j].pop]
 
@@ -228,28 +278,36 @@ class TestCell(unittest.TestCase):
         self.Matrix[0][1].population.allele_freq.side_effect = [0.7059, 0.1176]
         self.Matrix[1][2].population.allele_freq.side_effect = [0, 0.6875]
 
+        # Correctly returns all 4 lists with weighted values
         self.assertTupleEqual(self.Matrix[0][0].source_population_info(pop_list, self.Matrix, self.num_rows, self.num_cols, max_mig_rate), ([0.0276, 0.0], [0.7059, 0], [0.1176, 0.6875], [17, 32]))
 
     def test_weighting(self):
+        """Tests weighting method"""
 
+        # Correctly returns original value when values and factors don't vary
         values_list = [30, 30]
         factors_list = [10, 10]
         self.assertEqual(self.Matrix[0][0].weighting(values_list, factors_list), 30)
 
+        # Correctly return midpoint of values when one is 2x the other and factors doesn't vary
         values_list = [30, 60]
         factors_list = [10, 10]
         self.assertEqual(self.Matrix[0][0].weighting(values_list, factors_list), 45)
 
+        # Correctly returns weighted values when initial values and factors more complex
         values_list = [0.1176, 0.6875]
         factors_list = [17, 32]
         self.assertEqual(self.Matrix[0][0].weighting(values_list, factors_list), 0.4898)
 
+        # Correctly returns weighted values when one value is 0
         values_list = [0.0276, 0.0]
         factors_list = [17, 32]
         self.assertEqual(self.Matrix[0][0].weighting(values_list, factors_list), 0.0096)
 
     def test_freq_after_migration(self):
+        """Tests freq_after_migration method"""
 
+        # Correctly returns new allele frequency of mock_pop_2 based on migration from other populations
         self.Matrix[0][1].population.allele_freq.return_value = 0.1176
         migration_weighted = 0.0206
         allele_weighted = 0.6429
@@ -257,6 +315,7 @@ class TestCell(unittest.TestCase):
 
         self.assertEqual(self.Matrix[0][1].freq_after_migration(migration_weighted, allele_weighted, locus), 0.1284)
 
+        # Correctly returns new allele frequency of mock_pop_1 as current frequency if there is no migration.
         self.Matrix[0][0].population.allele_freq.return_value = 0.5
         migration_weighted = 0
         allele_weighted = 0.4898
