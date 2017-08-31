b4fcf31eff7da7f7818e1343e310f2e5ac2f77b9
James Santangelo <james.santangelo37@gmail.com>
2017-08-31 13:18:59 -0400

    Modified Merge script for merging files from simulations that vary K across the landscape

--- SEC_Code/Python/Merge_csv.py
+++ SEC_Code/Python/Merge_csv.py
@@ -1,7 +1,6 @@
 #!/usr/local/bin/python2.7
 
 # Import necessary modules
-import csv
 import sys as sys
 import os
 import glob as glob
@@ -10,50 +9,51 @@ import shutil
 
 
 def merge_csv():
-	'''Function merges csvs in directory.
+    '''Function merges csvs in directory.
 
-	Merges .csv files with a user specified parameter value
-	in it's name. Useful for merging only csv's with data
-	corresponding to specific simulations.
+    Merges .csv files with a user specified parameter value
+    in it's name. Useful for merging only csv's with data
+    corresponding to specific simulations.
 
-	Returns:
-	1. merged csv
-	'''
-	# Specify the parameter value for which datasets should be merged
-	param = sys.argv[1]
-	value = sys.argv[2]
-	parameter = param + value
+    Returns:
+    1. merged csv
+    '''
+    # Specify the parameter value for which datasets should be merged
+    # param = sys.argv[1]
+    # value = sys.argv[2]
+    # parameter = param + value
 
-	# Change directory to path containing .csv files to merge
-	os.chdir(sys.argv[3])
+    # Change directory to path containing .csv files to merge
+    os.chdir(sys.argv[1])
 
-	# Current date as local variable
-	datestring = datetime.strftime(datetime.now(), '%Y%m%d')
+    # Current date as local variable
+    datestring = datetime.strftime(datetime.now(), '%Y%m%d')
 
-	# Append all .csv files to python list
-	filelist = []
-	for csv in glob.glob("*.csv"):
-		if parameter in csv:
-			filelist.append(csv)
+    # Append all .csv files to python list
+    filelist = []
+    for file in glob.glob("*.csv"):
+        # if parameter in file:
+        filelist.append(file)
 
-	# Create file that will be the merged .csv file.
-	merged = open(datestring + "_" + sys.argv[4] + ".csv", "a")
+    # Create file that will be the merged .csv file.
+    merged = open(datestring + "_" + sys.argv[2] + ".csv", "a")
 
-	# For first .csv file in filelist, read all lines
-	for line in open(filelist[0]):
-		merged.write(line)
+    # For first .csv file in filelist, read all lines
+    for line in open(filelist[0]):
+        merged.write(line)
 
-	# Read all other .csv files, skipping the header.
-	for csv in filelist[1:len(filelist)]:
-		f = open(csv)
-		f.next() # skip the header
-		for line in f:
-			merged.write(line)
-		f.close()
-	merged.close()
+    # Read all other .csv files, skipping the header.
+    for file in filelist[1:len(filelist)]:
+        f = open(file)
+        f.next()  # skip the header
+        for line in f:
+            merged.write(line)
+        f.close()
+    merged.close()
 
-	for csv in filelist:
-		shutil.move(csv, "/scratch/research/projects/trifolium/SEC_Simulation.Evolutionary.Clines/SEC_Data/To_delete")
+    for file in filelist:
+        shutil.move(file, "/scratch/research/projects/trifolium/SEC_Simulation.Evolutionary.Clines/SEC_Data/To_delete")
+
 
 if __name__ == '__main__':
-	merge_csv() # Call function if script is run 'main'
+    merge_csv()  # Call function if script is run 'main'
