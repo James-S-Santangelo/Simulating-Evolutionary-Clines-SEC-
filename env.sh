b12c889e179ebcb87aa29efa8265e1d6764467af
James Santangelo <james.santangelo37@gmail.com>
2017-09-10 19:52:46 -0400

    Folder renaming and adding setup.py

--- env.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/Users/jamessantangelo/Documents/Academia/Doctorate_PhD/Projects/SEC_Simulating.evolutionary.clines/SEC_Git/SEC_Code/Python
-echo $PYTHONPATH
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Simulations',
+      version='1.0',
+      description='Simulating evolutionary clines',
+      author='James Santangelo',
+      author_email='james.santangelo37@gmail.com',
+      url='https://github.com/James-S-Santangelo/Simulating-Evolutionary-Clines-SEC-',
+      package_dir={'Simulations': 'SEC_Code/Simulations'},
+      packages=['Simulations'],
+      )
