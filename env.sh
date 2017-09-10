84ab9b81806bdb499cedf780a32da1edc5ff2bd3
James Santangelo <james.santangelo37@gmail.com>
2017-09-10 18:55:04 -0400

    Updating Environment shell script

--- env.sh
+++ env.sh
@@ -1,2 +1,1 @@
-export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:/Users/jamessantangelo/Documents/Academia/Doctorate_PhD/Projects/SEC_Simulating.evolutionary.clines/SEC_Git/SEC_Code/Python
