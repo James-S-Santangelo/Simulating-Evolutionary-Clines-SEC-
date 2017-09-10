db8a5706b86a55079ceecaf4ccd4c2801ac5f60b
James Santangelo <james.santangelo37@gmail.com>
2017-09-10 18:52:00 -0400

    Adding environment shell script

---
+++ env.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/Users/jamessantangelo/Documents/Academia/Doctorate_PhD/Projects/SEC_Simulating.evolutionary.clines/SEC_Git/SEC_Code/Python
