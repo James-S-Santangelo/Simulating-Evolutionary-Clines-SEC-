1a4c20df7e28f8f82382d2c827385e110eda0f42
James Santangelo <james.santangelo37@gmail.com>
2017-09-14 17:21:33 -0400

    Changed setup.py to reflect modified directory and package name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-setup(name='Simulations',
+setup(name='simulations',
       version='1.0',
       description='Simulating evolutionary clines',
       author='James Santangelo',
       author_email='james.santangelo37@gmail.com',
       url='https://github.com/James-S-Santangelo/Simulating-Evolutionary-Clines-SEC-',
-      package_dir={'Simulations': 'SEC_Code/Simulations'},
-      packages=['Simulations'],
+      package_dir={'simulations': 'SEC_Code/simulations'},
+      packages=['simulations'],
       )
