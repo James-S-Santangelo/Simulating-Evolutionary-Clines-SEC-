ab33c6e1db7174f0a2010722e06d5dc5f92ef2de
James Santangelo <james.santangelo37@gmail.com>
2017-09-14 20:31:37 -0400

    restucturing package

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='simulations',
       author='James Santangelo',
       author_email='james.santangelo37@gmail.com',
       url='https://github.com/James-S-Santangelo/Simulating-Evolutionary-Clines-SEC-',
-      package_dir={'simulations': 'SEC_Code/simulations'},
       packages=['simulations'],
       )
